d3fc8f358092848b7d668166969d4d74eaefb567
ChauAbdul786 <ChauAbdul786@gmail.com>
2021-10-08 23:36:12 -0700

    Finished Part 2 C Code

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
 /*	Author: Abdullah Chaudhry
  *  Partner(s) Name: 
  *	Lab Section: 021
- *	Assignment: Lab #3  Exercise #1
- *	Exercise Description: Count the number of 1s on ports A and B and output that number on port C
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: Fuel sensor on A3 - A0. C5 - C0 display the corresponding fuel level. C6 is a low fuel light that lights when the fuel is level 4 or less.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -15,28 +15,54 @@
 int main(void) {
 	/* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 
 	/* Insert your solution below */
 	unsigned char tmpA = 0x00;
-	unsigned char tmpB = 0x00;
 	unsigned char tmpC = 0x00;
-	unsigned char cnt = 0;
+
 	while (1) {
 		tmpA = PINA;
-		tmpB = PINB;
-		cnt = ((tmpA & 0x80) >> 7) + ((tmpA & 0x40) >> 6) + 
-					((tmpA & 0x20) >> 5) + ((tmpA & 0x10) >> 4) + 
-					((tmpA & 0x08) >> 3) + ((tmpA & 0x04) >> 2) + 
-					((tmpA & 0x02) >> 1) + ((tmpA & 0x01)) + 
-					((tmpB & 0x80) >> 7) + ((tmpB & 0x40) >> 6) + 
-					((tmpB & 0x20) >> 5) + ((tmpB & 0x10) >> 4) + 
-					((tmpB & 0x08) >> 3) + ((tmpB & 0x04) >> 2) + 
-					((tmpB & 0x02) >> 1) + ((tmpB & 0x01));
+		tmpC = PINC;
 
-		tmpC = cnt;
-		PORTC = tmpC;		
+		switch(tmpA){
+		case 0x00:
+			tmpC = 0x40; //Fuel Level 0 => 0100 000
+					//Not described in lab spec, assuming this is the intended behavior 
+			break;
+		case 0x01:
+		case 0x02:
+			tmpC = 0x60; //Fuel Level 1 or 2 => 0110 0000
+			break;
+		case 0x03:
+		case 0x04:
+			tmpC = 0x70;	//Fuel Level 3 or 4 => 0111 0000
+			break;
+		case 0x05:
+		case 0x06:
+			tmpC = 0x38; //Fuel Level 5 or 6 => 0011 1000
+			break;
+		case 0x07:
+		case 0x08:
+		case 0x09:
+			tmpC = 0x3C; //Fuel Level 7 - 9 => 0011 1100
+			break;
+		case 0x0A:
+		case 0x0B:
+		case 0x0C:
+			tmpC = 0x3E; //Fuel Level 10 - 12 => 0011 1110
+			break;
+		case 0x0D:
+		case 0x0E:
+		case 0x0F:
+			tmpC = 0x3F; //Fuel Level 13 - 15 => 0011 1111
+		default:
+			tmpC = 0x3F; //If none of these cases, 
+					//Fuel Level was likely mistakenly set > 15 => 0011 1111
+			break;
+		}
+	PORTC = tmpC;
+		
 	}
 	return 1;
 }
