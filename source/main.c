1e6a99f2e3150bc4e10c50fe340351ee124867a8
ChauAbdul786 <ChauAbdul786@gmail.com>
2021-10-08 21:55:43 -0700

    Initial Attempts for Part 1

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
-/*	Author: lab
+/*	Author: Abdullah Chaudhry
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 021
+ *	Assignment: Lab #3  Exercise #1
+ *	Exercise Description: Count the number of 1s on ports A and B and output that number on port C
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -13,11 +13,28 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+	/* Insert your solution below */
+	unsigned char tmpA = PINA;
+	unsigned char tmpB = PINB;
+	unsigned char tmpC = 0x00;
+	while (1) {
+		unsigned char cnt = ((tmpA & 0x80) >> 7) + ((tmpA & 0x40) >> 6) + 
+					((tmpA & 0x20) >> 5) + ((tmpA & 0x10) >> 4) + 
+					((tmpA & 0x08) >> 3) + ((tmpA & 0x04) >> 2) + 
+					((tmpA & 0x02) >> 1) + ((tmpA & 0x01)) + 
+					((tmpB & 0x80) >> 7) + ((tmpB & 0x40) >> 6) + 
+					((tmpB & 0x20) >> 5) + ((tmpB & 0x10) >> 4) + 
+					((tmpB & 0x08) >> 3) + ((tmpB & 0x04) >> 2) + 
+					((tmpB & 0x02) >> 1) + ((tmpB & 0x01));
 
-    }
-    return 1;
+		tmpC = cnt;		
+	}
+	
+	PORTC = tmpC;
+	return 1;
 }
